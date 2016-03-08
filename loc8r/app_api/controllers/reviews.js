5c51b6acce8df1f5df600b67bf7ff4ad014aad00
Steve King <stephen.king@enernoc.com>
2016-03-07 22:43:48 -0800

    cleanup

--- loc8r/app_api/controllers/reviews.js
+++ loc8r/app_api/controllers/reviews.js
@@ -102,39 +102,6 @@ module.exports.reviewsDeleteOne = function (req, res) {
 	});
 };
 
-//module.exports.reviewsDeleteOne = function (req, res) {
-//	if (!req.params.locationid || !req.params.reviewid) {
-//		sendJsonResponse(res, 404, {"message": "Not found, locationid and reviewid are both required"});
-//		return;
-//	}
-//	Loc.findById(req.params.locationid).select('reviews').exec(function(err, location) {
-//		if (!location) {
-//			sendJsonResponse(res, 404, {"message": "locationid not found"});
-//			return;
-//		} else if (err) {
-//			sendJsonResponse(res, 400, err);
-//			return;
-//		}
-//		if (location.reviews && location.reviews.length > 0) {
-//			if (!location.reviews.id(req.params.reviewid)) {
-//				sendJsonResponse(res, 404, {"message": "reviewid not found"});
-//			} else {
-//				location.reviews.id(req.params.reviewid).remove();
-//				location.save(function (err) {
-//					if (err) {
-//						sendJsonResponse(res, 404, err);
-//					} else {
-//						updateAverageRating(location._id);
-//						sendJsonResponse(res, 204, null);
-//					}
-//				});
-//			}
-//		} else {
-//			sendJsonResponse(res, 404, {"message": "No review to delete"});
-//		}
-//	});
-//};
-
 var sendJsonResponse = function(res, status, content) {
 	res.status(status);
 	res.json(content);
